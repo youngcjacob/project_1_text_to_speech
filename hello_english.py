3e035da7fccf933e14446807842c53a7529e2e1c
youngcjacob <72167849+youngcjacob@users.noreply.github.com>
2022-03-17 12:08:55 -0700

    Updated to include a line to delete the mp3 file

--- hello_english.py
+++ hello_english.py
@@ -1,5 +1,6 @@
 import gtts
 from playsound import playsound
+import os
 
 # makes request to google to get the synthesis
 tts = gtts.gTTS("Hello World")
@@ -9,3 +10,5 @@ tts.save("hello.mp3")
 
 # play the sound in the audio file
 playsound("hello.mp3")
+
+os.remove("hello.mp3")
